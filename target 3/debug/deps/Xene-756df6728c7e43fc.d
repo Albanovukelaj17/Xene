97264b74feeb1be0dfe037d3c8d59862182f988b
Albano <albigoal@outlook.com>
2024-10-10 20:10:24 +0200

    update gitignore

--- target 3/debug/deps/Xene-756df6728c7e43fc.d
+++
@@ -1,8 +0,0 @@
-/Users/albanovukelaj/Desktop/Motherfucker/Xene/target/debug/deps/Xene-756df6728c7e43fc: src/lib.rs src/lexer.rs src/parser.rs src/interpreter.rs
-
-/Users/albanovukelaj/Desktop/Motherfucker/Xene/target/debug/deps/Xene-756df6728c7e43fc.d: src/lib.rs src/lexer.rs src/parser.rs src/interpreter.rs
-
-src/lib.rs:
-src/lexer.rs:
-src/parser.rs:
-src/interpreter.rs:
